2881c0f30abe50555dc36e31e9c91a02e1a2cdf6
mehdi <m.mollaverdi@gmail.com>
2013-04-14 22:31:25 +1000

    Added a JS file providing fixed array data structure. Modified httpServer module to serve js files as well. Fixed array JS is downloaded from https://github.com/Ravenwall/node-fixed-array

--- httpServer.js
+++ httpServer.js
@@ -1,15 +1,34 @@
 var httpServer = require('http').createServer(handler)
-  ,fs = require('fs');
+  ,fs = require('fs')
+  ,url = require('url');
+  
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
 
 function handler (request, response) {
-  fs.readFile(__dirname + '/index.html', function (error, data) {
-    if (error) {
-      response.writeHead(500);
-      return response.end('Internal Server Error');
-    }
-    response.writeHead(200);
-    response.end(data);
-  });
+  var pathname = url.parse(request.url).pathname;
+  console.log('pathname: ' + pathname);
+  if (pathname.endsWith('.js')) {
+    fs.readFile(__dirname + pathname, function (error, data) {
+      if (error) {
+        response.writeHead(500);
+        return response.end('Internal Server Error');
+      }
+      response.writeHead(200);
+      response.end(data);
+    });
+  } else {
+    fs.readFile(__dirname + '/index.html', function (error, data) {
+      if (error) {
+        response.writeHead(500);
+        return response.end('Internal Server Error');
+      }
+      response.writeHead(200);
+      response.end(data);
+    
+    });
+  }
 };
 
 exports.start = function() {
