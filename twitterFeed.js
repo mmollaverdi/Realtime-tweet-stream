2e0701ae93005fce0b63d92dfdb1f7aa157513f8
mehdi <m.mollaverdi@gmail.com>
2013-04-14 13:53:11 +1000

    Fixed the way credentials were used

--- twitterFeed.js
+++ twitterFeed.js
@@ -1,6 +1,12 @@
-var credentials = require('./twitterCredentials').credentials;
+var credentials = require('./twitterCredentials');
 var ntwitter = require('ntwitter');
 var twitter = new ntwitter(credentials);
+
+twitter.verifyCredentials(function (error, data) {
+  if (error) {
+    console.log('Credential verification error: ' + error);
+  }
+});
 
 exports.currentTweets = function(emit) {
   emit('Test Content');
@@ -10,11 +16,16 @@ exports.newTweets = function(emit) {
 
   twitter.stream(
     'statuses/filter',
-    { track: ['awesome', 'cool', 'rad', 'gnarly', 'groovy'] },
+    { track: ['nodejs', 'javascript', 'AngularJS', 'EmberJS', 'WebSocket'] },
     function(stream) {
+
       stream.on('data', function(tweet) {
-        console.log(tweet.text);
+        console.log('New tweet: ' + tweet.text);
         emit(tweet.text);
+      });
+      
+      stream.on('error', function(error, code) {
+        console.log("Steram error: " + error + ": " + code);
       });
     }
   );
